024a0cea70eda293d06884ada87e8d31d00b947f
Alvin Hsu <alvinhsu88@gmail.com>
2022-07-13 12:18:34 -0700

    CSS and JS minify
    Minified all CSS and JS files.

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,17 +1,1 @@
-$(document).ready(function(slickSlide){
-    $('.mockups').slick({
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      draggable: true,
-      dots: true,
-      arrows: true,
-      responsive: [
-        {
-          breakpoint: 768,
-          settings: {
-            arrows: false,
-          }
-        }
-      ]
-    });
-  });
+$(document).ready(function(a){$(".mockups").slick({slidesToShow:1,slidesToScroll:1,draggable:!0,dots:!0,arrows:!0,responsive:[{breakpoint:768,settings:{arrows:!1}}]})})
